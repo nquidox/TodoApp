c75d9230cbc30824897e527f63b19b2224030dc2
nquidox <nquidox@gmail.com>
2024-06-26 19:16:17 +0300

    ref

--- api/user/routes.go
+++ api/user/routes.go
@@ -1,15 +1,14 @@
 package user
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func AddRoutes(router *http.ServeMux) {
-	router.HandleFunc("POST /api/v1/create", CreateUserHandler)
-	router.HandleFunc(fmt.Sprintf("GET /api/v1/read/{id}"), ReadUserHandler)
-	router.HandleFunc("UPDATE /api/v1/update/{id}", UpdateUserHandler)
-	router.HandleFunc("DELETE /api/v1/delete/{id}", DeleteUserHandler)
-	router.HandleFunc("POST /api/v1/login", LoginHandler)
-	router.HandleFunc("GET /api/v1/logout", LogoutHandler)
+	router.HandleFunc("POST /api/v1/user", CreateUserHandler)
+	router.HandleFunc("GET /api/v1/user/{id}", ReadUserHandler)
+	router.HandleFunc("UPDATE /api/v1/user/{id}", UpdateUserHandler)
+	router.HandleFunc("DELETE /api/v1/user/{id}", DeleteUserHandler)
+	router.HandleFunc("POST /api/v1/user/login", LoginHandler)
+	router.HandleFunc("GET /api/v1/user/logout", LogoutHandler)
 }
