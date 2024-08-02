2155971dcc47622f6eb00bc5acea55bbe8ae5257
nquidox <nquidox@gmail.com>
2024-08-02 18:03:17 +0300

    auth fixes

--- main.go
+++ main.go
@@ -9,26 +9,25 @@ import (
 	"todoApp/db"
 )
 
-//	@title			TODO App API
-//	@version		1.0
-//	@description	This is an educational TODO App API, clone of IT-Incubator's API written in Go.
-
-//	@contact.name	API Support
-//	@contact.url	https://t.me/rekasawak
-
-//	@license.name	MIT
-//	@license.url	https://mit-license.org/
-
-//	@host		localhost:9000
-//	@BasePath	/api/v1
-
-//	@SecurityDefinition	CookieAuth
-//	@in					cookie
-//	@name				token
-
+//	@title						TODO App API
+//	@version					1.0
+//	@description				This is an educational TODO App API, clone of IT-Incubator's API written in Go.
+//
+//	@contact.name				API Support
+//	@contact.url				https://t.me/rekasawak
+//
+//	@license.name				MIT
+//	@license.url				https://mit-license.org/
+//
+//	@host						localhost:9000
+//	@BasePath					/api/v1
+//
+//	@securityDefinitions.basic	BasicAuth
+//	@in							header
+//	@name						token
+//
 //	@externalDocs.description	OpenAPI
 //	@externalDocs.url			https://swagger.io/resources/open-api/
-
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
